eda441ba19ba109a6f2409f4dbeaba756346f9c4
Benjamin Thom <jaminthom@gmail.com>
2020-02-25 19:16:24 -0800

    Modify the Car class to include a message about the car and display it with the other info about the car, Add functionality to only show cars that are under a certain price and mileage. Modify the method in the application so the user can enter a number for each maximum value, and Add an if statement to the Main() method so a message is displayed if no cars match the budget constraint

--- Cars.cs
+++ Cars.cs
@@ -6,17 +6,26 @@ public class Car
   public string MakeModel;
   public int Price;
   public int Miles;
+  public string Message;
 
-  public Car(string makeModel, int price, int miles)
+  public Car(string makeModel, int price, int miles, string message)
   {
     MakeModel = makeModel;
     Price = price;
     Miles = miles;
+    Message = message;
   }
 
-  public bool WorthBuying(int maxPrice)
+  public bool WorthBuying(int maxPrice, int maxMiles)
   {
-    return (Price < maxPrice);
+    if (Miles < maxMiles)
+    {
+      return (Price < maxPrice);
+    }
+    else
+    {
+      return false;
+    }
   }
 }
 
@@ -24,30 +33,39 @@ public class Program
 {
   public static void Main()
   {
-    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-    Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-    Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-    Car amc = new Car("1976 AMC Pacer", 400, 198000);
+    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, "Best damn car I ever owned.");
+    Car yugo = new Car("1980 Yugo Koral", 700, 56000, "The real reason I opened this lot.");
+    Car ford = new Car("1988 Ford Country Squire", 1400, 239001, "The Babysitter Special!");
+    Car amc = new Car("1976 AMC Pacer", 400, 198000, "Rally Racer in the making.");
 
     List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
 
     Console.WriteLine("Enter maximum price: ");
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
+    Console.WriteLine("Enter maximum mileage: ");
+    string stringMaxMiles = Console.ReadLine();
+    int maxMiles = int.Parse(stringMaxMiles);
 
     List<Car> CarsMatchingSearch = new List<Car>(0);
 
     foreach (Car automobile in Cars)
     {
-      if (automobile.WorthBuying(maxPrice))
+      if (automobile.WorthBuying(maxPrice, maxMiles))
       {
         CarsMatchingSearch.Add(automobile);
       }
     }
-
+  if (CarsMatchingSearch.Count > 0)
+  {
     foreach(Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.MakeModel);
     }
+  }
+  else
+  {
+    Console.WriteLine("There is nothing for you here. Which is sad. We have a Koral.");
+  }
   }
 }
